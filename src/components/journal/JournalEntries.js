c7e73539f545401d366a1ab3eea7896b47ecfb13
lalo1234321 <edominguezc@toluca.tecnm.mx>
2021-01-31 21:27:30 -0600

    new corrections on the sidebar

--- src/components/journal/JournalEntries.js
+++ src/components/journal/JournalEntries.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { JournalEntry } from './JournalEntry';
 
 export const JournalEntries = () => {
-    const entries = [1,2,3,4,5,6,7,8,9,10];
-
+    const entries = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+    // const entries = [1,2,3,4];
     return (
         <div className="journal__entries">
              {
--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -5,8 +5,27 @@ export const JournalEntry = () => {
         <div className="journal__entry">
             <div 
                 className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://images.unsplash.com/photo-1522865080725-2a9ea1fcb94e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max)'
+                }}
             >
+            </div>
 
+            <div className="journal__entry-body">
+                <p className="journal__entry-title">
+                    Un nuevo día
+                </p>
+
+                <p className="journal__entry-content">
+                    Aliqua deserunt officia duis nostrud anim veniam incididunt.
+                </p>
+
+            </div>
+
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
             </div>
         </div>
     )
