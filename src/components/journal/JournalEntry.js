185359d144107980c4763ed4c4bee30b3c6a7ec8
lalo1234321 <edominguezc@toluca.tecnm.mx>
2021-01-31 20:20:03 -0600

    correcting the scroll bar error in sass

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -3,7 +3,11 @@ import React from 'react'
 export const JournalEntry = () => {
     return (
         <div className="journal__entry">
-            
+            <div 
+                className="journal__entry-picture"
+            >
+
+            </div>
         </div>
     )
 }
